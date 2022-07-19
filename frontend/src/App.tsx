8f3df9c8575a820ad328fb0cf3b1f4fdf37b145d
sergiorbotelho <sergiorbotelho@gmail.com>
2022-07-19 10:59:34 -0300

    Toast

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,12 @@
 import Header from "./components/Header";
-import NotificationButton from "./components/NotoficationButton";
 import Card from "./components/Card";
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 function App() {
   return (
     <>
+      <ToastContainer/>
       <Header />
       <main>
         <section id="sales">
